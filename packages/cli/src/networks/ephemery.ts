8778beaecd7539b2754794f4d0b3e5f87456699b
atkinsonholly <atkinsonholly@gmail.com>
2023-10-06 15:56:50 +0100

    add: WIP ephemery genesisFileUrl using ephemery export

--- packages/cli/src/networks/ephemery.ts
+++ packages/cli/src/networks/ephemery.ts
@@ -1,7 +1,16 @@
+import path from "node:path";
+import {generateGenesis} from "@lodestar/ephemery/genesis";
+
 export {ephemeryChainConfig as chainConfig} from "@lodestar/config/networks";
 
 export const depositContractDeployBlock = 0;
-export const genesisFileUrl = "https://ephemery.dev/latest/genesis.ssz";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+generateGenesis();
+
+const BASE_OUT_PATH = path.join(__dirname, "../../out/genesis");
+
+export const genesisFileUrl = `${BASE_OUT_PATH}/genesis.ssz`; // || "https://ephemery.dev/latest/genesis.ssz";
 export const bootnodesFileUrl = "https://ephemery.dev/latest/bootstrap_nodes.txt";
 
 // Pick from above file
