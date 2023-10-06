87133786cd31f16cccdd9faff0db664f630cd229
atkinsonholly <atkinsonholly@gmail.com>
2023-10-06 16:09:50 +0100

    add: WIP ephemery cli option, generateGenesis

--- packages/cli/src/networks/ephemery.ts
+++ packages/cli/src/networks/ephemery.ts
@@ -1,16 +1,20 @@
 import path from "node:path";
+import {fileURLToPath} from "node:url";
 import {generateGenesis} from "@lodestar/ephemery/genesis";
 
 export {ephemeryChainConfig as chainConfig} from "@lodestar/config/networks";
 
-export const depositContractDeployBlock = 0;
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// TODO: link generateGenesis to flag
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-generateGenesis();
+// generateGenesis();
 
-const BASE_OUT_PATH = path.join(__dirname, "../../out/genesis");
+const BASE_OUT_PATH = path.join(__dirname, "../../../ephemery/out/genesis");
 
-export const genesisFileUrl = `${BASE_OUT_PATH}/genesis.ssz`; // || "https://ephemery.dev/latest/genesis.ssz";
+export const depositContractDeployBlock = 0;
+export const genesisFileUrl = `${BASE_OUT_PATH}/genesis.ssz` || "https://ephemery.dev/latest/genesis.ssz";
 export const bootnodesFileUrl = "https://ephemery.dev/latest/bootstrap_nodes.txt";
 
 // Pick from above file
